1119d986d1b40cde2d873d2d9d92a54b150aa9b9
kd-ut <kdut683@gmail.com>
2026-08-27 21:55:29 +0800

    前端TS改为JS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import './styles.css'
+import './assets/main.css'
 import App from './App.vue'
 import router from './router'
 
