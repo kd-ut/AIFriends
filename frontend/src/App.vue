87c34d24180c9616a915d13bbc12639a428d06db
kd-ut <kdut683@gmail.com>
2026-08-28 15:57:18 +0800

    创建导航栏

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
+<script setup>
+import NavBar from "@/components/navbar/NavBar.vue";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <NavBar>
+    页面内容
+  </NavBar>
+
 </template>
 
 <style scoped></style>
